e4fb85a4c10e52bb2c16911f8e18fbba3302af1c
Chijindu <cnnwakama@gmail.com>
2021-04-09 19:59:03 +0100

    Added functions and optimized more

--- python ATM.py
+++ python ATM.py
@@ -1,54 +1,113 @@
-name = input("What is your name? \n" )
 
 from datetime import datetime
 now= datetime.now()
 
-approved_names=["Emeka", "Deji" , "Mike"]
-approved_passwords= ["passwordEmeka", "passwordDeji","passwordMike"]
+import random
+database={}
 
 
-if(name in approved_names):
-    password= input("Please input your password \n")
-    user_id = approved_names.index(name)
+def init():
+    is_valid_option = False 
 
-    if(password == approved_passwords[user_id]):
-        print("Welcome back %s" %name)
-        
-        date_time = now.strftime("%d/%m/%Y %H:%M:%S")
-        print("Todays date and time are ",date_time )
+    print("Welcome to Simba Bank")
+    while is_valid_option == False:
 
-        print("These are the options:")
-        print("1. Withdrawal")
-        print("2. Deposit")
-        print("3. Complaint")
-        
-        atm_input=input("How can we help you today? \n")
-        if(atm_input == "1"):
-            input("How much would you like to withdraw? \n")
-            print("Take your cash.")
-            pass
-        
-        elif(atm_input == "2"):
-            deposit=input("How much would you like to deposit? \n")
-            print("Your current balance is $%s." %deposit)
-            pass
-        
-        elif(atm_input == "3"):
-             input("What issue will you like to report? \n")
-             print("Thank you for contacting us.")
-             pass
+        have_acct=input("Do you have an account with us? 1 (yes) 2 (no) \n")
+        if(have_acct == '1'):
+            is_valid_option = True
+            login()
+            
+        elif(have_acct == '2'):
+            is_valid_option = True
+            print(register())  
+            
         else:
-            print("Invalid option selected")
+            print("Invalid option selected")  
+
+
+def login():
+    
+    name = input("What is your name? \n" )
+
+    approved_names=["Emeka", "Deji" , "Mike"]
+    approved_passwords= ["passwordEmeka", "passwordDeji","passwordMike"]
+
+
+    if(name in approved_names):
+        password= input("Please input your password \n")
+        user_id = approved_names.index(name)
+
+        if(password == approved_passwords[user_id]):
+            print("Welcome back %s" %name)
+            
+            date_time = now.strftime("%d/%m/%Y %H:%M:%S")
+            print("Todays date and time are ",date_time )
+            bank_operations()
+
+        else:
+            print("Incorrect password, please try again.")
+            init()
+            
     else:
-        print("Incorrect password, please try again.")
-        
-else:
-    print("You are not an approved user")
-    
+        print("You are not an approved user.")
+        init()
+            
 
         
-    
+def register():
+  will_register = input("You do not have an account with us, would you like to register? 1 (yes) 2 (no) \n")
+  if(will_register == '1'):
+    first_name = input("What is your first name? \n")
+    last_name =input("What is your last name? \n")
+    email =input("What is your email address? \n")
+    password =input("Create a password \n")
+    account_num = generate_acct_num()
 
+    database[account_num] = [first_name, last_name, email]
+
+    print("Your account has been created")
+
+    login()
+
+  elif(will_register == '2'):
+    print("Please come back again sometime.")
+
+  else:
+    print("Invalid option selected")
+    init()  
+
+def generate_acct_num():
+  print("Generating account number...")
+  return random.randrange(0000000000,9999999999)
+
+def bank_operations():
+    
+            print("These are the options:")
+            print("1. Withdrawal")
+            print("2. Deposit")
+            print("3. Complaint")
+            
+            atm_input=input("How can we help you today? \n")
+            if(atm_input == "1"):
+                input("How much would you like to withdraw? \n")
+                print("Take your cash.")
+                pass
+            
+            elif(atm_input == "2"):
+                deposit=input("How much would you like to deposit? \n")
+                print("Your current balance is $%s." %deposit)
+                pass
+            
+            elif(atm_input == "3"):
+                input("What issue will you like to report? \n")
+                print("Thank you for contacting us.")
+                pass
+            else:
+                print("Invalid option selected")
+                init()
+  
+   
+init()
 
 
 
